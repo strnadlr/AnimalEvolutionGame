f0bfed2264cee1183686adb8e01b2e967144e495
strnadlr <rstrnadlova@gmail.com>
2021-12-01 22:31:05 +0100

    Revert "Fixed bug where without generating a terain there would be a different mesh and meshfilter for terrain"
    This reverts commit 7d213e3c52e699886c443ee59c9d840dfde4ad9a.

--- AnimalEvolution/Assets/Entities/UIEntityCreation.cs
+++ AnimalEvolution/Assets/Entities/UIEntityCreation.cs
@@ -1,29 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class UIEntityCreation : MonoBehaviour
 {
-    public Text nutriValueText;
-    public Text timeBeOfText;
-    public Text sizeText;
-    public Text mutationStrText;
-
-    public GameObject panel;
-    private bool active = false;
-    
-
     // Start is called before the first frame update
-    private void Start()
+    void Start()
     {
-        panel.SetActive(active);
-    }
-
-    public void EntityUIButtonClicked()
-    {
-        active = !active;
-        panel.SetActive(active);
+        
     }
 
     // Update is called once per frame
